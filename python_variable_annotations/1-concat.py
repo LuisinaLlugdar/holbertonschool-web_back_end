17472f3c4419dbf88ce3ae26fb263995014d498d
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-08-29 13:37:08 -0700

    correct pycodestyle :)

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,6 +2,7 @@
 """type-annotated function concat that takes a string str1
 and a string str2 as arguments and returns a concatenated string"""
 
+
 def concat(str1: str, str2: str) -> str:
     """Returns the concatenation of two strings"""
     return str1 + str2
--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -3,6 +3,7 @@
 and returns the floor of the float"""
 import math
 
+
 def floor(n: float) -> int:
     """Returns the floor of a float"""
     return math.floor(n)
